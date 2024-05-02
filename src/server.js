20deed6d476891dc7c05acd4d0111da23e90bdde
Chris Hebert <chebert@nvidia.com>
2024-05-02 13:30:08 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,16 +11,13 @@ http.createServer(function (req, res) {
         var qdata = q.query;
         sensor_value =  qdata.sensor_value;
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end("Setting Sensor Value");
+        res.end("OK");
+      console.log("Setting Sensor Value : " + sensor_value);
     break;
     case "/get":
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('Getting Sensor Value  ' + sensor_value);
+        res.end('value=' + sensor_value);
         console.log(sensor_value);
     break;
   }
-}).listen(8080);
-
-http://192.168.1.210:8080/set?sensor_value=75
-
-http://192.168.1.210:8080/get
+}).listen(9090);
